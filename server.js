b275d8cc3d5e5036da5718740a13283032c96605
Kaitlin Hill <hillkai@flip2.engr.oregonstate.edu>
2018-06-08 22:09:00 -0700

    updated stuff

--- server.js
+++ server.js
@@ -16,11 +16,58 @@ app.get('/home', function(req, res, next){
   res.status(200).render('homePage');
 });
 
-app.get('/joinusPage', function(req, res, next){
+app.get('/joinus', function(req, res, next){
   res.status(200).render('joinusPage');
 });
 
+var temp = [2];
+temp[0]={
+  companyName: "Something",
+  companyContent: "More stuff",
+  fullURL: "this",
+  niceURL: "that",
+  imagePath: "images/amazon.png"
+}
+temp[1]={
+  companyName: "Something More",
+  companyContent: "Even More stuff",
+  fullURL: "thisisim",
+  niceURL: "that is more im",
+  imagePath: "images/eecs.png"
+}
 
+
+app.get('/partnerships', function(req, res, next){
+  res.status(200).render('collabPage',{
+    company: temp
+  });
+});
+var comment =[2]
+comment[0] =  {
+  author: "bob",
+  commentContent: "asdklfhewaejasjdbfjasd"
+}
+comment[1] =  {
+  author: "Susan",
+  commentContent: "Great photo!"
+}
+var tempt = [2];
+ tempt[0] = {
+  title: "this blog",
+  imgpath: "images/outreach.png",
+  content: "this is a little bit of text"
+}
+tempt[1] = {
+ title: "this  is also great",
+ imgpath: "images/amazon.png",
+ content: "even more text to test this thing"
+}
+app.get('/blog', function(req, res, next){
+  res.status(200).render('blogPage',{
+    blogInfo: tempt,
+    comment: comment
+  })
+});
 /*app.get('/twits/:n', function(req, res, next){
   if(req.params.n>-1 && req.params.n<8){
     res.status(200).render('twitsPage',{
