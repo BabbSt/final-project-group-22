6a1c663da52f0b6677946f600f8f850e60ba78d6
Stephanie Babb <babbst@oregonstate.edu>
2018-06-11 08:46:07 -0700

    Teying to get Mongo working

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var MongoClient = require('mongodb').MongoClient;
 var app = express();
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || '27017' ;
+var mongoUsername = process.env.MONGO_USERNAME;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoURL="mongodb://"+ mongoUsername+ ":"+ mongoPassword+ "@"+ mongoHost+ ":"+ mongoPort+ "/"+ mongoDBName;
+
+var mongoDB =null;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -38,10 +49,18 @@ temp[1]={
 
 
 app.get('/partnerships', function(req, res, next){
-  res.status(200).render('collabPage',{
-    company: temp
+  var companiesCollection=mongoDB.collection('companies');
+  companiesCollection.find().toArray(function (err, companies) {
+      if (err) {
+            res.status(500).send("Error fetching companies from DB.");
+	 } else {
+  		res.status(200).render('collabPage',{
+    			company: companies
+  		});
+	}
   });
 });
+
 var comment =[2]
 comment[0] =  {
   author: "bob",
@@ -68,17 +87,6 @@ app.get('/blog', function(req, res, next){
     comment: comment
   })
 });
-/*app.get('/twits/:n', function(req, res, next){
-  if(req.params.n>-1 && req.params.n<8){
-    res.status(200).render('twitsPage',{
-      twits: [twitsObjects[req.params.n]],
-      showButton: false
-    });
-  }
-  else{
-    next();
-  }
-});*/
 
 app.use(express.static('public'));
 
@@ -87,6 +95,16 @@ app.get('*', function (req, res) {
 });
 
 
-app.listen(port, function () {
+/*app.listen(port, function () {
   console.log("== Server is listening on port", port);
+});*/
+
+MongoClient.connect(mongoURL, function (err, client) {
+  if (err) {
+      throw err;
+    }
+  mongoDB = client.db(mongoDBName);
+    app.listen(mongoPort, function () {
+        console.log("== Server listening on port", mongoport);
+	  });
 });
