21eeec3faf3bbe7a90b9d6e1073ab3bfba64e205
Kaitlin Hill <hillkai@flip3.engr.oregonstate.edu>
2018-06-11 22:01:52 -0700

    deleted temporary testing vars

--- server.js
+++ server.js
@@ -31,23 +31,6 @@ app.get('/joinus', function(req, res, next){
   res.status(200).render('joinusPage');
 });
 
-var temp = [2];
-temp[0]={
-  companyName: "Something",
-  companyContent: "More stuff",
-  fullURL: "this",
-  niceURL: "that",
-  imagePath: "images/amazon.png"
-}
-temp[1]={
-  companyName: "Something More",
-  companyContent: "Even More stuff",
-  fullURL: "thisisim",
-  niceURL: "that is more im",
-  imagePath: "images/eecs.png"
-}
-
-
 app.get('/partnerships', function(req, res, next){
   var companiesCollection=mongoDB.collection('companies');
   companiesCollection.find().toArray(function (err, companies) {
@@ -60,24 +43,7 @@ app.get('/partnerships', function(req, res, next){
 	}
   });
 });
-/*  author: "bob",
-  commentContent: "asdklfhewaejasjdbfjasd"
-}
-comment[1] =  {
-  author: "Susan",
-  commentContent: "Great photo!"
-}*/
-var tempt = [2];
- tempt[0] = {
-  title: "this blog",
-  imgpath: "images/outreach.png",
-  content: "this is a little bit of text"
-}
-tempt[1] = {
- title: "this  is also great",
- imgpath: "images/amazon.png",
- content: "even more text to test this thing"
-}
+
 app.get('/blog', function(req, res, next){
   var blogCollection=mongoDB.collection('blogContent');
   var commentCollection=mongoDB.collection('comments');
